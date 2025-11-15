cf9149975deb7f125519005cff4315ff21b56b12
lcorso1234 <lawrencecorso1@gmail.com>
2025-11-15 16:28:37 -0600

    Launch

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ISP Next Starter",
+  description: "Next.js + Tailwind CSS starter for ISP project"
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
